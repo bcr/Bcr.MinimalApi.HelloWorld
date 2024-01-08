a6943785c2b22de83fca89177b17641152333e1e
Blake Ramsdell <blaker@gmail.com>
2024-01-08 02:22:14 -0800

    Add startup and shut down logging

--- Bcr.MinimalApi.HelloWorld/Program.cs
+++ Bcr.MinimalApi.HelloWorld/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,19 @@ builder.Host.UseSerilog((context, config) =>
 
 var app = builder.Build();
 
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
+// Log the application start
+{
+    var assembly = Assembly.GetExecutingAssembly();
+    var appName = assembly.GetName().Name;
+    var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "unknown";
+
+    logger.LogInformation("Starting {AppName} {Version}", appName, version);
+}
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+logger.LogInformation("Application shut down");
